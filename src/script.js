9c16016571478d75e5c137bd2ae9ce68bffca1fb
JordanC <jcwilliams410@gmail.com>
2020-06-07 22:15:39 -0400

    logic cleanup update

--- src/script.js
+++ src/script.js
@@ -1,75 +1,59 @@
-//Choices and players
-let choices = ["Rock", "Paper", "Scissors"];
+//Choices
+const ROCK = "Rock";
+const PAPER = "Paper";
+const SCISSORS = "Scissors"
+let choices = [ROCK, PAPER, SCISSORS];
+
+//Opponents
 const player = {
-  playerChoice: null
+  Choice: null
 };
 
 const computer = {
-  computerChoice: null
+  Choice: null
 };
 
-//Choices logic
+//Computer logic
 const computerChooses = () => {
   return choices[Math.floor(Math.random() * choices.length)];;
 }
 
-//Winner space in HTML
-let winnerNotification = document.createElement('p'); //create paragraph for message
+//Winner notification container
+let winnerNotification = document.querySelector('#results'); //assign paragraph to winner notification
 let referenceNode = document.querySelector('.buttons'); //create reference for paragraph placement
 referenceNode.after(winnerNotification); //place paragraph after reference
 
+//Winner notification text
+function generateResultsText(winner, playerChoice, computerChoice) {
+  let message = `The ${winner} wins! The player chose ${playerChoice} and the computer chose ${computerChoice}.`
+  return message;
+}
+
 //Game simulation
 const compareChoices = () => {
-  //Winner notifications
-  let playerWins = "The player wins! The player chose " + player.playerChoice + " and the computer chose " + computer.computerChoice + ".";
-  let computerWins = "The computer wins! The player chose " + player.playerChoice + " and the computer chose " + computer.computerChoice + ".";
   
-  //tie  
-  if (computer.computerChoice == player.playerChoice) {
-    winnerNotification.innerText = "It's a tie! Both the player and the computer chose " + computer.computerChoice + ".";
-  }
-
-  //not tie
-  else if (computer.computerChoice === choices[0]) {
-      if (player.playerChoice === choices[1]) {
-        winnerNotification.innerText = playerWins;
-      } else if (player.playerChoice === choices[2]) {
-        winnerNotification.innerText = computerWins;
-      }
-  } else if (computer.computerChoice === choices[1]) {
-    if (player.playerChoice === choices[0]) {
-      winnerNotification.innerText = computerWins;
-    } if (player.playerChoice === choices[2]) {
-      winnerNotification.innerText = playerWins;
-    }
-  } else if (computer.computerChoice === choices[2]) {
-    if (player.playerChoice === choices[1]) {
-      winnerNotification.innerText = computerWins;
-    } else if (player.playerChoice = choices[0]) {
-      winnerNotification.innerText = playerWins;
-    }
+  //Conditional winner notification text
+  if (computer.Choice == player.Choice) {
+    winnerNotification.innerText = `It's a tie! Both the player and the computer chose ${computer.Choice}.`;
+  } else if (player.Choice === ROCK && computer.Choice === SCISSORS) {
+    winnerNotification.innerText = generateResultsText("player", player.Choice, computer.Choice);
+  } else if (player.Choice === PAPER && computer.Choice === ROCK) {
+    winnerNotification.innerText = generateResultsText("player", player.Choice, computer.Choice);
+  } else if (player.Choice === SCISSORS & computer.Choice === PAPER) {
+    winnerNotification.innerText = generateResultsText("player", player.Choice, computer.Choice);
+  } else {
+    winnerNotification.innerText = generateResultsText("computer", player.Choice, computer.Choice);
   }
 }
 
-function playerChooses() {
-    document.querySelector('#rock').onclick = function() {
-    winnerNotification.innerText = null;
-    player.playerChoice = choices[0];
-    computer.computerChoice = computerChooses();
-    compareChoices();
-  }
-  document.querySelector('#paper').onclick = function() {
-    winnerNotification.innerText = null;
-    player.playerChoice = choices[1];
-    computer.computerChoice = computerChooses();
-    compareChoices();
-  }
-  document.querySelector('#scissors').onclick = function() {
-    winnerNotification.innerText = null;
-    player.playerChoice = choices[2];
-    computer.computerChoice = computerChooses();
-    compareChoices();
-  }
+
+const onClick = (e) => {
+  winnerNotification.innerText = null;
+  player.Choice = e.target.innerText;
+  computer.Choice = computerChooses()
+  compareChoices();
 }
 
-playerChooses();
+document.querySelector('#rock').addEventListener('click', onClick);
+document.querySelector('#paper').addEventListener('click', onClick);
+document.querySelector('#scissors').addEventListener('click', onClick);
